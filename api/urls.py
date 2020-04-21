cf8412811f139885bc99b5b28ddb67c71f02c2f4
IrinaRostovtseva <ni8003@yandex.ru>
2020-04-21 13:38:46 +0300

    Adds url patterns for posts, comments, group and follow requests.

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comment')
+router.register(r'group', GroupViewSet, basename='group')
+router.register(r'follow', FollowViewSet, basename='follow')
 
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
